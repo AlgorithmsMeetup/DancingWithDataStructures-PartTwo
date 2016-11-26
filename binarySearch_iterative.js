4082cb494a8fc1b6034bdd3dfb1704554ac9e07d
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-11-26 11:00:48 -0800

    Remove extra lines

--- binarySearch_iterative.js
+++ binarySearch_iterative.js
@@ -33,10 +33,3 @@ println("Found prime at index " + result);
 Program.assertEqual(doSearch(primes, 73), 20);
 Program.assertEqual(doSearch(primes, 7), 3);
 Program.assertEqual(doSearch(primes, 83), 22);
-Program.assertEqual(doSearch(primes, 73), 20);
-
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
-var result = doSearch(primes, 73);
-console.log("Found prime at index " + result);
