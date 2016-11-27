191d238941d42d712ff0ac49aa0ec8a4a9dfde66
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-11-26 20:53:42 -0800

    Add recursive solution'

---
+++ binarySearch_recursive.js
@@ -0,0 +1,27 @@
+var doSearch = function(array, target, min, max) {
+  min = min || 0;
+  max = max || array.length - 1;
+  var guess = Math.floor((max + min) / 2);
+  
+  if (array[guess] === target) {
+    return guess;
+  }
+
+  if (min > max) {
+    return -1;
+  }
+
+  return array[guess] > target ? 
+    doSearch(array, target, min, guess - 1) : 
+    doSearch(array, target, guess + 1, max);
+  
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+console.assert(doSearch(primes, 73)); // 20
+console.assert(doSearch(primes, 7)); // 3
+console.assert(doSearch(primes, 83)); // 22
+console.assert(doSearch(primes, 6)); // -1
+console.assert(doSearch(primes, 84)); // -1
